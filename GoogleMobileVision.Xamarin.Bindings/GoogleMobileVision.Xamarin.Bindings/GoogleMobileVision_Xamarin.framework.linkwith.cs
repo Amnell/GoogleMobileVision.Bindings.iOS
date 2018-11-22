0dfb25aa70d05ad8fb2ff8f90120cd4a9a7b14b0
Mathias Amnell <mathias.amnell@ffcg.se>
2018-11-22 09:29:39 +0100

    Add Xamarin bindings project

---
+++ GoogleMobileVision.Xamarin.Bindings/GoogleMobileVision.Xamarin.Bindings/GoogleMobileVision_Xamarin.framework.linkwith.cs
@@ -0,0 +1,6 @@
+﻿using ObjCRuntime;
+
+[assembly: LinkWith("GoogleMobileVision_Xamarin.framework")]
+[assembly: LinkWith("GTMSessionFetcher.framework")]
+[assembly: LinkWith("Protobuf.framework")]
+[assembly: LinkWith("GoogleToolboxForMac.framework")]
